19061b1f8c9c810e2b83165809a6234224e3c656
Ivan Sishchuk <ivan.sishchuk@inventorsoft.co>
2023-03-15 10:01:06 +0100

    Formatted IinParserIntegrationTest

--- src/test/java/com/capgemini/iinparser/integration/IinParserIntegrationTest.java
+++ src/test/java/com/capgemini/iinparser/integration/IinParserIntegrationTest.java
@@ -13,66 +13,83 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Slf4j
 class IinParserIntegrationTest {
 
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private final String ERROR_MESSAGE = "Unsupported PAN format";
+    private ByteArrayOutputStream outputStream;
+    private static final String ERROR_MESSAGE = "Unsupported PAN format";
 
     @BeforeEach
-    void setup(){
+    void setup() {
+        outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
         System.setErr(new PrintStream(outputStream));
     }
 
     @Test
-    void shouldReturnFormattedPanForVisa(){
-        Main.main(new String[] { "4444444444444444" });
+    void shouldReturnFormattedPanForVisa() {
+        Main.main(new String[]{"4444444444444444"});
+
         String consoleOutput = outputStream.toString();
         String expectedOutput = "4444 4444 4444 4444";
+
         assertTrue(consoleOutput.contains(expectedOutput));
     }
 
     @Test
-    void shouldReturnErrorTooLongPan(){
-        Main.main(new String[] { "44444444444444444444" });
+    void shouldReturnErrorTooLongPan() {
+        Main.main(new String[]{"44444444444444444444"});
+
         String consoleOutput = outputStream.toString();
+
         assertTrue(consoleOutput.contains(ERROR_MESSAGE));
     }
 
     @Test
-    void shouldReturnErrorTooSmallPan(){
-        Main.main(new String[] { "44" });
+    void shouldReturnErrorTooSmallPan() {
+        Main.main(new String[]{"44"});
+
         String consoleOutput = outputStream.toString();
+
         assertTrue(consoleOutput.contains(ERROR_MESSAGE));
     }
 
     @Test
-    void shouldReturnFormattedPanForDiners(){
-        Main.main(new String[] { "30122994494222" });
+    void shouldReturnFormattedPanForDiners() {
+        Main.main(new String[]{"30122994494222"});
+
         String consoleOutput = outputStream.toString();
         String expectedOutput = "3012 299449 4222";
+
         assertTrue(consoleOutput.contains(expectedOutput));
     }
 
     @Test
-    void shouldReturnFormattedPanForMaestro(){
-        Main.main(new String[] { "5000015123563212456" });
+    void shouldReturnFormattedPanForMaestro() {
+        Main.main(new String[]{"5000015123563212456"});
+
         String consoleOutput = outputStream.toString();
         String expectedOutput = "5000 0151 2356 3212 456";
+
         assertTrue(consoleOutput.contains(expectedOutput));
     }
 
     @Test
-    void shouldReturnNotChangedPan(){
+    void shouldReturnNotChangedPan() {
         String expectedOutput = "444444444444444";
-        Main.main(new String[] { expectedOutput });
+
+        Main.main(new String[]{expectedOutput});
+
         String consoleOutput = outputStream.toString();
+
         assertTrue(consoleOutput.contains(expectedOutput));
     }
 
     @Test
-    void shouldReturnDefaultPan(){
+    void shouldReturnDefaultPan() {
         String expectedOutput = "4444 4444 4444 4444";
-        Main.main(new String[] { });
+
+        Main.main(new String[]{});
+
         String consoleOutput = outputStream.toString();
+
         assertTrue(consoleOutput.contains(expectedOutput));
     }
 }
